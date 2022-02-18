f3180d7deb4190ebd98a8e53cfc224cf2a5fbb18
Samuel Yanes <45543903+derpberk@users.noreply.github.com>
2022-02-18 13:38:27 +0100

    Added results of other algorithms

---
+++ Results/EntropyMinimizationResults/StaticComparisonResults.py
@@ -0,0 +1,43 @@
+import pandas
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+# Mix results #
+
+metric = 'MSE_SVR'
+df_drl = pandas.read_csv('./DRL_noisy_static.csv')
+df_drl['Algorithm'] = 'DRL'
+df_lm = pandas.read_csv('./LawnMowerResults')
+df_lm['Algorithm'] = 'Lawn Mower'
+df_rn = pandas.read_csv('./StaticFullRandom.csv')
+df_rn['Algorithm'] = 'Random'
+df_rc = pandas.read_csv('./StaticRandomCoverage.csv')
+df_rc['Algorithm'] = 'Coverage'
+df_gr = pandas.read_csv('./std_greedy.csv')
+df_gr['Algorithm'] = 'Greedy $\sigma$'
+
+all_data = pandas.concat([df_drl,df_lm,df_rn,df_rc,df_gr], ignore_index=True)
+all_data['Distance'] = all_data['Distance']*45/200
+
+sns.set_theme()
+sns.set(rc={'figure.figsize':(6,4)})
+g = sns.lineplot(data=all_data, x='Distance', y=metric, hue='Algorithm', style='Algorithm')
+
+g.set(ylabel='Mean Squared Error')
+g.set(xlabel='Distance $(km)$')
+g.set(xlim=(0, 45))
+g.set(yscale='log')
+
+final_metrics = all_data.loc[all_data['Distance'] >= 45]
+
+for algorithm_name in final_metrics['Algorithm'].unique():
+
+	algorithm_metrics = final_metrics.loc[(final_metrics['Algorithm'] == algorithm_name)]
+	print(f"###### ALGORITHM {algorithm_name} #######")
+
+	for metric_name in ['Reward', 'Entropy', 'A_vector', 'DetectionRate', 'MSE_GP', 'MSE_SVR']:
+
+		print(f'Value of {metric_name}: {algorithm_metrics[metric_name].mean()} +- {algorithm_metrics[metric_name].std()}')
+
+
+plt.show()
---
+++ Results/metrics_loader.py
@@ -0,0 +1,76 @@
+from sklearn.gaussian_process.kernels import RBF, ConstantKernel as C
+from sklearn.gaussian_process import GaussianProcessRegressor
+from sklearn.svm import SVR
+from sklearn.metrics import mean_squared_error
+import copy
+import pandas
+
+class metric_constructor:
+
+	def __init__(self, path):
+
+		self.path = path
+		# Metrics
+		self.R = []
+		self.DH = []
+		self.A = []
+		self.XI = []
+		self.DI = []
+		self.MSE_GP = []
+		self.MSE_SVR = []
+
+		kernel = C(1.0) * RBF(length_scale=5)
+		self.gp = GaussianProcessRegressor(kernel=kernel, alpha=1E-5)
+		self.svr = SVR(C=1E5)
+		self.data_vector = []
+
+
+	def record_new(self):
+
+		self.R = []
+		self.DH = []
+		self.A = []
+		self.XI = []
+		self.DI = []
+		self.MSE_GP = []
+		self.MSE_SVR = []
+
+	def record_step(self, r, dh, a, xi, di, meas_locs, meas_vals, visitable_locs, true_vals):
+
+		self.R.append(r)
+		self.DH.append(dh)
+		self.A.append(a)
+		self.XI.append(xi)
+		self.DI.append(di)
+
+		# Fit and Obstain model #
+		self.gp.fit(meas_locs, meas_vals)
+		self.svr.fit(meas_locs, meas_vals)
+		self.MSE_GP.append(mean_squared_error(y_true=true_vals, y_pred=self.gp.predict(visitable_locs)))
+		self.MSE_SVR.append(mean_squared_error(y_true=true_vals, y_pred=self.svr.predict(visitable_locs)))
+
+
+	def record_finish(self, t):
+
+		data = {'Run': t,
+		        'Reward': self.R,
+		        'Entropy': self.DH,
+		        'A_vector': self.A,
+		        'DetectionRate': self.XI,
+		        'Distance': self.DI,
+		        'MSE_GP': self.MSE_GP,
+		        'MSE_SVR': self.MSE_SVR}
+
+		dataframe = pandas.DataFrame.from_dict(data)
+
+		self.data_vector.append(dataframe)
+
+	def record_save(self, path=None):
+
+		if path is None:
+			path = self.path
+
+		dataframe_final = pandas.concat(self.data_vector, ignore_index=True)
+		dataframe_final.to_csv(path)
+
+
